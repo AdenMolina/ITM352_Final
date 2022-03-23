2d23840c48cc99beec24412d6eddb001636d7184
AdenMolina <adenm@hawaii.edu>
2022-03-22 17:33:23 -1000

    need to fix (security)

--- Molina_Aden_Assignment2/server.js
+++ Molina_Aden_Assignment2/server.js
@@ -16,11 +16,19 @@ products_array.forEach( (prod,i) => {prod.total_sold = 0});
 const { response } = require('express');
 
 var app = express();
+var errors={};
 
 app.use(myParser.urlencoded({ extended: true }));
 
-
-
+if (fs.existsSync(filename)) {
+    data = fs.readFileSync(filename, 'utf-8');
+    user_data = JSON.parse(data); //turns data into string
+    fileStats = fs.statSync(filename);
+    console.log("File " + filename + " has been properly loaded containing " + fileStats.size + " characters");
+}
+else {
+    console.log("Please enter a correct filename.");
+}
 
 // monitor all requests
 app.all('*', function (request, response, next) {
@@ -57,6 +65,8 @@ app.get("/store", function (request, response) {
 
         for (i = 0; i < products_array.length; i++) {
             str += `
+            <form name="product_selection_form" action="" method="POST">
+
                 <section class="item">
                     <h2>${products_array[i].album}</h2>
                     <h3>${products_array[i].artist}</h3>
@@ -64,8 +74,8 @@ app.get("/store", function (request, response) {
                     <h3>$${products_array[i].price}</h3>
                     <label id="quantity${i}_label"}">Quantity:</label>
                     <div class="box">
-                    <input type="number" placeholder="Enter a Quantity"  name="quantity${i}" 
-                     required min="0" max="15" onkeyup="checkQuantityTextbox(this); ">
+                    <input type="number" placeholder="Enter a Quantity"  name="${products_array[i].quantity}" 
+                      min="0" max="15" onkeyup="checkQuantityTextbox(this); ">
                     </div>
                     <div class="Inventory">
                     <h3>${products_array[i]["total_sold"]} ${products_array[i]["album"]} have been sold!</h3>
@@ -77,8 +87,154 @@ app.get("/store", function (request, response) {
     }
 
 });   
+app.post("/store", function (request, response) {
+    // Redirect to login page with form data in query string
+    let params = new URLSearchParams(request.body);
+   
+
+   //let product = params.toString();
+
+   //if (typeof params.toString(`quantity${i}`))
+
+   for (i = 0; i < products_array.length; i++) {
+   if (params.has(`${products_array[i].quantity}`)){
+    a_qty = params.get(`${products_array[i].quantity}`);
+    if (a_qty  > 0) {
+        response.redirect('./register?'+ params.toString());
+    }
+    else if (a_qty != 0 ) {
+        response.redirect('./store?'+ params.toString());
+    }
+
+   
+    //response.send(`${a_qty}`)
+
+   }
+}
+   
+
+   
+    
+
+
+    
+    
+    //response.redirect('./register?'+ params.toString());
+});
+
+
+
+
+app.get("/login", function (request, response) {
+
+let params = new URLSearchParams(request.query);
+
+var contents = fs.readFileSync('./views/login.html', 'utf8');
+response.send(eval('`' + contents + '`')); // render template string
+
+
+
+    function display_login() {
+    str = `
+    <body>
+    <form action="?${params.toString()}" method="POST">
+    <input type="text" name="username" size="40" placeholder="Enter Username" ><br />
+    <input type="password" name="password" size="40" placeholder="Enter Password"><br />
+    <input type="submit" value="Submit" id="submit">
+    </form>
+    </body>
+        `;
+        return str;
+    }
+    });
+    
+    app.post("/login", function (request, response) {
+        // Process login form POST and redirect to logged in page if ok, back to login page if not
+        let params = new URLSearchParams(request.query);
+
+        the_username = request.body['username'].toLowerCase();
+        the_password = request.body['password'];
+        if (typeof user_data[the_username] != 'undefined') {
+            if (user_data[the_username].password == the_password) {
+                response.redirect('./process_invoice?'+ params.toString());
+
+            } else {
+                response.send(`Wrong password!`);
+            }
+            return;
+        }
+        response.send(`${the_username} does not exist`);
+    });
+    // user info JSON file
+
+
+
+
+app.get("/register", function (request, response) {
+    let params = new URLSearchParams(request.query);
+
+    var contents = fs.readFileSync('./views/register.html', 'utf8');
+response.send(eval('`' + contents + '`')); // render template string
+
+// Give a simple register form
+
+function display_register(){
+        str = `
+<body>
+
+<form action="" method="POST">
+<input type="text" name="username" required minlength="4" maxlength="8" size="40" placeholder="Enter Username" > 
+${ (typeof errors['no_username'] != 'undefined')?errors['no_username']:''}
+${ (typeof errors['username_taken'] != 'undefined')?errors['username_taken']:''}
+<br />
+<input type="password" name="password" required minlength="6" size="40" placeholder="Enter Password"><br />
+<input type="password" name="repeat_password" required minlength="6" size="40" placeholder="Enter Password Again">
+${ (typeof errors['password_mismatch'] != 'undefined')?errors['password_mismatch']:''}
+<br />
+<input type="email" name="email" size="40" placeholder="Enter Email"><br />
+<input type="submit" value="Submit" id="submit">
+<p><b><a href="./login?'+ ${params.toString()}" target="_blank">Got an Account?</a></b></p>
+</form>
+</body>
+    `;
+return str;
+}
+});
+
+app.post("/register", function (request, response) {
+    let params = new URLSearchParams(request.query);
+
+
+    // process a simple register form
+    username = request.body.username.toLowerCase();
+
+    // check is username taken
+    if(typeof user_data[username] != 'undefined') {
+        errors['username_taken'] = `Hey! ${username} is already registered!`;
+        response.redirect('./register?'+ params.toString());
+    }
+    if(request.body.password != request.body.repeat_password) {
+        errors['password_mismatch'] = `Repeat password not the same as password!`;
+        response.redirect('./register?'+ params.toString());
+    } 
+    if(request.body.username == '') {
+        errors['no_username'] = `You need to select a username!`;
+        response.redirect('./register?'+ params.toString());
+    }
+    if(Object.keys(errors).length == 0) {
+        user_data[username] = {};
+        user_data[username].password = request.body.password;
+        user_data[username].email = request.body.email;
+        fs.writeFileSync(filename, JSON.stringify(user_data));
+        console.log("Saved: " + user_data);
+        response.redirect('./process_invoice?'+ params.toString());
+
+    } else {
+        response.redirect("./register");
+    }
+});
 //From Assignment1_MVC_server, creates invoice
-app.post("/process_invoice", function (request, response, next) {
+app.get("/process_invoice", function (request, response, next) {
     let POST = request.body;
     if (typeof POST['purchase_submit'] == 'undefined') {
         console.log('No purchase form data');
@@ -96,8 +252,8 @@ app.post("/process_invoice", function (request, response, next) {
         str = ''; 
         for (i = 0; i < products_array.length; i++) {
             a_qty = 0; 
-            if(typeof POST[`quantity${i}`] != 'undefined') { // if statement to see if posted value is not equals to undefined
-                a_qty = POST[`quantity${i}`]; //if it is true, the value is assined to a_qty
+            if(typeof request.query[`quantity${i}`] != 'undefined') { // if statement to see if posted value is not equals to undefined
+                a_qty = request.query[`quantity${i}`]; //if it is true, the value is assigned to a_qty
             }
             if (a_qty > 0) { // if statement to see if a_qty is greater than 0, creates product row
                 extended_price =a_qty * products_array[i].price //extended price is a_qty mulitplied by price of producct
@@ -142,98 +298,6 @@ app.post("/process_invoice", function (request, response, next) {
     }
  
 });
-app.get("/login", function (request, response) {
-var contents = fs.readFileSync('./views/login.html', 'utf8');
-response.send(eval('`' + contents + '`')); // render template string
-
-
-    function display_login() {
-    str = `
-    <body>
-    <form action="" method="POST">
-    <input type="text" name="username" size="40" placeholder="enter username" ><br />
-    <input type="password" name="password" size="40" placeholder="enter password"><br />
-    <input type="submit" value="Submit" id="submit">
-    </form>
-    </body>
-        `;
-    response.send(str);
-    }
-    });
-    
-    app.post("/login", function (request, response) {
-        // Process login form POST and redirect to logged in page if ok, back to login page if not
-        the_username = request.body['username'].toLowerCase();
-        the_password = request.body['password'];
-        if (typeof users_reg_data[the_username] != 'undefined') {
-            if (users_reg_data[the_username].password == the_password) {
-                response.send(`User ${the_username} is logged in`);
-            } else {
-                response.send(`Wrong password!`);
-            }
-            return;
-        }
-        response.send(`${the_username} does not exist`);
-    });
-    // user info JSON file
-
-
-if (fs.existsSync(filename)) {
-    var stats = fs.statSync(filename);
-    data = fs.readFileSync(filename, 'utf-8');
-    users_reg_data = JSON.parse(data);
-} else {
-    console.log(filename + ' does not exist!');
-    users_reg_data = {};
-}
-
-app.get("/register", function (request, response) {
-// Give a simple register form
-        str = `
-<body>
-<form action="" method="POST">
-<input type="text" name="username" size="40" placeholder="enter username" > 
-${ (typeof errors['no_username'] != 'undefined')?errors['no_username']:''}
-${ (typeof errors['username_taken'] != 'undefined')?errors['username_taken']:''}
-<br />
-<input type="password" name="password" size="40" placeholder="enter password"><br />
-<input type="password" name="repeat_password" size="40" placeholder="enter password again">
-${ (typeof errors['password_mismatch'] != 'undefined')?errors['password_mismatch']:''}
-<br />
-<input type="email" name="email" size="40" placeholder="enter email"><br />
-<input type="submit" value="Submit" id="submit">
-</form>
-</body>
-    `;
-    response.send(str);
-    
-});
-
-app.post("/register", function (request, response) {
-    // process a simple register form
-    username = request.body.username.toLowerCase();
-
-    // check is username taken
-    if(typeof users_reg_data[username] != 'undefined') {
-        errors['username_taken'] = `Hey! ${username} is already registered!`;
-    }
-    if(request.body.password != request.body.repeat_password) {
-        errors['password_mismatch'] = `Repeat password not the same as password!`;
-    } 
-    if(request.body.username == '') {
-        errors['no_username'] = `You need to select a username!`;
-    }
-    if(Object.keys(errors).length == 0) {
-        users_reg_data[username] = {};
-        users_reg_data[username].password = request.body.password;
-        users_reg_data[username].email = request.body.email;
-        fs.writeFileSync(filename, JSON.stringify(users_reg_data));
-        console.log("Saved: " + users_reg_data);
-        response.send(`${username} has been registered.`);
-    } else {
-        response.redirect("./register");
-    }
-});
 
     
     var listener = app.listen(8080, () => { console.log('server started listening on port ' + listener.address().port) });
@@ -241,4 +305,4 @@ app.post("/register", function (request, response) {
 app.use(express.static('./public'));
 
 // start server
-app.listen(8080, () => console.log(`listening on port 8080`));
+app.listen(8081, () => console.log(`listening on port 8080`));
